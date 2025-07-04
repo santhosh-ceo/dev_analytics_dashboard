fdf0d3558877b3849e232e24d02ff5e2c8c47952
Santhosh Adiga U <santhosh.adiga@anakramy.com>
2025-07-04 17:21:29 +0530

    deleted unwanted files

--- lib/dev_analytics_dashboard.dart
+++ lib/dev_analytics_dashboard.dart
@@ -1,7 +1,3 @@
-import 'dev_analytics_dashboard_platform_interface.dart';
 
-class DevAnalyticsDashboard {
-  Future<String?> getPlatformVersion() {
-    return DevAnalyticsDashboardPlatform.instance.getPlatformVersion();
-  }
-}
+
+
